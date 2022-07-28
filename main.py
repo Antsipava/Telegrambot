29f14035fdc249d5f02370ae0b774967167d336c
Julia Sergeevna <3695134@mail.ru>
2022-07-28 12:26:45 +0300

    сделаны кнопки

--- main.py
+++ main.py
@@ -4,28 +4,31 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
 import telebot
 import random
+import config
 from telebot import types
 # Загружаем список помещений
 f = open('data/pom.txt', 'r', encoding='UTF-8')
-pom = f.read().split('\n')
+pom = f.read()#.split('\n')
 f.close()
 
 # Создаем бота
-bot = telebot.TeleBot('5561287317:AAHEEFuA3rllmtw008dpaNK3Gn5okZpRxdk')# Здесь мой токен, полученный от @botfather
+bot = telebot.TeleBot(config.TOKEN)# Здесь мой токен, полученный от @botfather
+
+
+
 
 # Команда start
-
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
 
@@ -38,7 +41,9 @@ def start(m, res=False):
         markup.add(item2)
         markup.add(item3)
         bot.send_message(m.chat.id, 'Привет, я бот 2022, помогу, чем смогу \n Нажми: \nИщу свободное помещение  \nПоказать контакт управляющего зданием \nОтправить свой контакт ',  reply_markup=markup)
-
+# def star(message):
+#     mess = f'Need help?'
+#     bot.send_message(message.chat.id, mess)
 @bot.message_handler(commands=["geophone"])
 def geophone(message):
     # Эти параметры для клавиатуры необязательны, просто для удобства
@@ -47,19 +52,31 @@ def geophone(message):
     keyboard.add(button_phone)
     bot.send_message(message.chat.id, "Отправь мне свой номер телефона и мы вам перезвоним! ", reply_markup=keyboard)
 
+
+
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     # Если юзер прислал 1, выдаем ему список свободных помещений
     if message.text.strip() == 'Ищу свободное помещение' :
-            answer = random.choice(pom)
+            # numpom = 1
+            answer = pom
     # Если юзер прислал 2, выдаем контакт
     elif message.text.strip() == 'Контакт управляющего зданием':
             answer = '+375293695134 Юлия Сергеевна'
     elif message.text.strip() == 'Перезвоните мне':
             answer = geophone(message)
+    else :
+        answer = bot.send_message('Кнопки чуть ниже чата, выбирите из предложенных') # если не выбрали кнопку
     # Отсылаем юзеру сообщение в его чат
     bot.send_message(message.chat.id, answer)
+
+#создали перезапуск программы Menu
+bot.set_my_commands([
+    telebot.types.BotCommand("/start", "Перезапуск бота")
+
+])
+
 
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
